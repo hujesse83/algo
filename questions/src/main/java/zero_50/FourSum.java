90642ba9b999c730ad0eed081d3172b1d7d1e04a
hujesse83 <jesse.hu@liveramp.com>
2021-10-09 01:06:07 +0800

    sum

---
+++ questions/src/main/java/zero_50/FourSum.java
@@ -0,0 +1,38 @@
+package zero_50;
+
+import java.util.HashMap;
+
+/**
+ * 454
+ * 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+ */
+
+public class FourSum {
+    public static void main(String[] args) {
+        int A[] = {1,2};
+        int B[] = {-1,-2};
+        int C[] = {-1,2};
+        int D[] = {0,2};
+        System.out.println(fourSumCount(A, B, C, D));
+    }
+    public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //统计两个数组中的元素之和，同时统计出现的次数，放入map
+        for (int a : A) {
+            for (int b : B) {
+                // 这句话真的太叼了
+                map.put(a+b,map.get(a+b)==null?1:map.get(a+b)+1);
+            }
+        }
+        int counts = 0;
+        for (int i : C) {
+            for (int j : D) {
+                if(map.containsKey(-i-j)==true){
+                    counts+=map.get(-i-j);
+                }
+            }
+        }
+        return counts;
+    }
+
+}
--- questions/src/main/java/zero_50/TwoSum.java
+++ questions/src/main/java/zero_50/TwoSum.java
@@ -2,13 +2,14 @@ package zero_50;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 第一题
  * 1) 暴力穷尽法
  * 2）排序 + 头尾双指针寻找  但是排序之后顺序会变
  * 3) dp 写不出来
- * 4） hashmap ok
+ * 4） hashmap ok 是用数组作为哈希表来解决哈希问题
  */
 
 public class TwoSum {
@@ -22,20 +23,30 @@ public class TwoSum {
     /**
      * hashmap 以空间换时间
      */
-    public static int[] twoSum(int[] array, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
+
         // 建立k-v ，一一对应的哈希表
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < array.length; i++) {
-            if (map.containsKey(array[i])) {
-                indices[1] = i;
-                indices[0] = map.get(array[i]);
-                break;
-            } else {
-                // 将数据存入 key为补数 ，value为下标
-                map.put(target - array[i], i);
+        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(hash.containsKey(nums[i])){
+                indices[0] = i;
+                indices[1] = hash.get(nums[i]);
+                return indices;
             }
+            // 将数据存入 key为补数 ，value为下标
+            hash.put(target-nums[i],i);
         }
+        // // 双重循环 循环极限为(n^2-n)/2
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = nums.length - 1; j > i; j --){
+        //         if(nums[i]+nums[j] == target){
+        //            indexs[0] = i;
+        //            indexs[1] = j;
+        //            return indexs;
+        //         }
+        //     }
+        // }
         return indices;
     }
 
