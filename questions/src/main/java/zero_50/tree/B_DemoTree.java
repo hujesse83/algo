dc4c251230836908ebc66faecca4dac56fa1bdf0
hujesse83 <jesse.hu@liveramp.com>
2021-10-29 22:50:58 +0800

    begin tree unRecursion traverse

--- questions/src/main/java/zero_50/tree/B_DemoTree.java
+++ questions/src/main/java/zero_50/tree/B_DemoTree.java
@@ -56,6 +56,21 @@ public class B_DemoTree {
         System.out.println(node.val);
         infixOrder(node.right);
     }
+    //非递归
+    void infixOrderV2(B_DemoTree node){
+        B_DemoTree temp = node;
+        Stack<B_DemoTree> stack = new Stack<>();
+        while (temp!=null || !stack.isEmpty()){
+            while (temp!=null){
+                stack.push(temp);
+                temp = temp.left;
+            }
+            if (!stack.isEmpty()){
+                System.out.println(stack.pop().val);
+                temp = temp.right;
+            }
+        }
+    }
     //Binary Tree Preorder Traversal LC145
     void postOrder(B_DemoTree node) {
         if (node == null) {
@@ -64,6 +79,25 @@ public class B_DemoTree {
         postOrder(node.left);
         postOrder(node.right);
         System.out.println(node.val);
+    }
+    // 非递归
+    public static void postOrderV2(B_DemoTree root) {
+        Stack<B_DemoTree> stack1 = new Stack<>();
+        Stack<B_DemoTree> stack2 = new Stack<>();
+        stack1.push(root);
+        while (!stack1.isEmpty()) {
+            B_DemoTree temp = stack1.pop();
+            stack2.push(temp);
+            if(temp.left!=null){
+                stack1.push(temp.left);
+            }
+            if(temp.right!=null){
+                stack1.push(temp.right);
+            }
+        }
+        while (!stack2.isEmpty()) {
+            System.out.println(stack2.pop().val);
+        }
     }
 
 }
