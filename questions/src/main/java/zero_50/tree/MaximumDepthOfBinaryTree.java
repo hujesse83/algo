dd2943b61295de80f84fe2949e806e91a6a53f6a
hujesse83 <jesse.hu@liveramp.com>
2021-10-31 14:32:30 +0800

    层级遍历

--- questions/src/main/java/zero_50/tree/MaximumDepthOfBinaryTree.java
+++ questions/src/main/java/zero_50/tree/MaximumDepthOfBinaryTree.java
@@ -4,7 +4,22 @@ package zero_50.tree;
 public class MaximumDepthOfBinaryTree {
 
     public int maxDepth(TreeNode root) {
-return 1;
+        if (root==null){
+            return 0;
+        }
+        if (root.left==null && root.right==null){
+            return 1;
+        }
+        if (root.left!=null && root.right!=null){
+            return Math.min(maxDepth(root.left),maxDepth(root.right))+1;
+        }
+        if (root.left==null && root.right!=null){
+            return maxDepth(root.right)+1;
+        }
+        if (root.right==null && root.left!=null){
+            return maxDepth(root.left)+1;
+        }
+        return 1;
     }
 
 
