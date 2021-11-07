9e9c90631841491371586705008fbb6811bf6587
hujesse83 <jesse.hu@liveramp.com>
2021-11-07 15:56:40 +0800

    左叶子节点之和

---
+++ questions/src/main/java/zero_50/tree/SumOfLeftLeaves.java
@@ -0,0 +1,36 @@
+package zero_50.tree;
+
+/**
+ * @Time 2021/11/7 15：55
+ * LC404
+ */
+public class SumOfLeftLeaves {
+    // 自己野方法 ... 设置了左节点flag为1，右节点flag为2；
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        return dfs(root.left,1)+dfs(root.right,0);
+
+    }
+    public  int dfs(TreeNode root,int flag){
+        if (root==null){
+            return 0;
+        }
+        if (root.left==null && root.right==null && flag==1){
+            return root.val;
+        }
+        return dfs(root.left,1) + dfs(root.right,0);
+    }
+    // 官方解法！！
+    public int sumOfLeftLeavesV2(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        int res = 0;
+        if (root.left!=null && root.left.left==null &&root.left.right==null){
+            res +=root.left.val;
+        }
+        return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right)+res;
+    }
+}
