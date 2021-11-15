d2053bd621b76a840ad76937387d77eacba87fda
hujesse83 <jesse.hu@liveramp.com>
2021-11-15 16:45:35 +0800

    回朔

---
+++ questions/src/main/java/zero_50/backtrace/CombinationSumIII.java
@@ -0,0 +1,32 @@
+package zero_50.backtrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LC 216 在1-10中选出k个数，sum = n
+ * I made it on my own
+ */
+public class CombinationSumIII {
+    public List<List<Integer>> res = new ArrayList<>();
+    public List<Integer> item = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        backTrace(1,k,n);
+        return res;
+    }
+    // begin[1,10] k = 个数 n为总和
+    public void backTrace(int begin,int k,int n){
+        if(k==item.size() && n==0){
+            res.add(new ArrayList<>(item));
+            return;
+        }
+        if (n<0){
+            return;
+        }
+        for (int i = begin; i <10 ; i++) {
+            item.add(i);
+            backTrace(i+1,k,n-i); // 这样保证了begin是升序，相当于剪枝了
+            item.remove(item.size()-1);
+        }
+    }
+}
--- questions/src/main/java/zero_50/backtrace/Combinations.java
+++ questions/src/main/java/zero_50/backtrace/Combinations.java
@@ -6,27 +6,39 @@ import java.util.List;
 /**
  * @Description 组合
  * @Time 2021/11/14 23:19
- *擦 想不明白... 明日再战
+ * 擦 想不明白... 明日再战
+ * @Time 2021/11/15 11:21
+ * 我们需要区分排列和组合，组合没有顺序 [1,2] = [2,1]
  */
 public class Combinations {
-    public List<List<Integer>> res = new ArrayList<>();
-    public List<List<Integer>> combine(int n, int k) {
+    // V1 没做剪枝操作的 16ms
+    public static List<List<Integer>> res = new ArrayList<>();
+    public static List<Integer> item = new ArrayList<>();
+
+    public static List<List<Integer>> combine(int n, int k) {
         ArrayList<Integer> item = new ArrayList<>();
-        for (int i = 1; i <n ; i++) {
-            backTrace(i,n,k,item);
-        }
+        backTrace(1, n, k);
         return res;
     }
-    public void backTrace(int begin,int n,int k,List<Integer> item){
-        if (begin==n){
+
+    public static void backTrace(int begin, int n, int k) {
+        if (item.size() == k) {
+            ArrayList<Integer> temp = new ArrayList<>(item);
+            res.add(temp);
             return;
         }
-        item.add(begin);
-        if (item.size()==k){
-            ArrayList<Integer> newItem = new ArrayList<>(item);
-            res.add(newItem);
-            item.remove(item.size()-1);
+        // 减枝的操作就在这里 n-(k-item.size())+1 表示至多起始位置
+        for (int i = begin; i <= n - (k - item.size()) + 1; i++) {
+            item.add(i);
+            backTrace(i + 1, n, k);
+            item.remove(item.size() - 1);
         }
-        backTrace(begin+1,n,k,item);
     }
+
+    public static void main(String[] args) {
+        combine(4, 4);
+    }
+    /**
+     * 剪枝算是非常重要的操作了 从16ms到1ms。
+     */
 }
