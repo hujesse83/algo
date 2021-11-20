dbb6a8400c2a6283f7babce31c8f9ab169afff59
hujesse83 <jesse.hu@liveramp.com>
2021-11-21 01:03:29 +0800

    组合 去重:star

--- questions/src/main/java/zero_50/backtrace/CombinationSumII.java
+++ questions/src/main/java/zero_50/backtrace/CombinationSumII.java
@@ -15,35 +15,47 @@ import java.util.List;
  * [1,7],
  * [2,6]
  * ]
- * 难点在于去重..
+ * 难点在于去重..  v1:通过index
+ * v2:通过used数组 适用条件更广
  */
 public class CombinationSumII {
     public List<List<Integer>> res = new ArrayList<>();
     public List<Integer> item = new ArrayList<>();
+    public int[] used;
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        used = new int[candidates.length];
         Arrays.sort(candidates);
-        if (candidates.length==0){
+        if (candidates.length == 0) {
             return res;
         }
-        backTrace(candidates,target,0);
+        backTrace(candidates, target, 0);
         return res;
     }
-    public void backTrace(int []candidates,int target,int index){
-        if (target==0){
+
+    public void backTrace(int[] candidates, int target, int index) {
+        if (target == 0) {
             res.add(new ArrayList<>(item));
             return;
         }
-        for (int i = index; i <candidates.length ; i++) {
-            if (target-candidates[i]<0){
+        for (int i = index; i < candidates.length; i++) {
+            if (target - candidates[i] < 0) {
                 return;
             }
-            item.add(candidates[i]);
             // 很难的地方在于如何去重... 保证本层不重复（） 结合树状图联想
-            if (i>index && candidates[i]==candidates[i-1]){
+            /**
+             if (i > index && candidates[i] == candidates[i - 1]) {
+             continue;
+             }
+             */
+            if (i > 0 && used[i] == 0 && candidates[i] == candidates[i - 1]) {
                 continue;
             }
-            backTrace(candidates,target-candidates[i],i+1);
-            item.remove(item.size()-1);
+            item.add(candidates[i]);
+            used[i] = 1;
+            backTrace(candidates, target - candidates[i], i + 1);
+            item.remove(item.size() - 1);
+            used[i] = 0;
         }
     }
 }
