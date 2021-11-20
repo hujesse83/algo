003d020a9a93f0d2fbfa3307f99d0eb974ca3715
hujesse83 <jesse.hu@liveramp.com>
2021-11-20 21:32:45 +0800

    全排列 去重:star

---
+++ questions/src/main/java/zero_50/backtrace/Permutations.java
@@ -0,0 +1,38 @@
+package zero_50.backtrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author huJesse
+ * @Date 2021/11/20 21:17
+ * @Description 这个英语单词意思是全排列 LC46
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ */
+public class Permutations {
+    public List<List<Integer>> res = new ArrayList<>();
+    public List<Integer> item = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        backTrace(nums);
+        return res;
+    }
+
+    public void backTrace(int[] nums) {
+        if (item.size() == nums.length) {
+            res.add(new ArrayList<>(item));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 重复元素判断
+            if (item.contains(nums[i])) {
+                continue;
+            }
+            item.add(nums[i]);
+            backTrace(nums);
+            item.remove(item.size() - 1);
+        }
+    }
+
+}
