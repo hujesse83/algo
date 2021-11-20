199514db12fafbd13d125975802d53666e402d88
hujesse83 <jesse.hu@liveramp.com>
2021-11-21 01:12:12 +0800

    组合 去重:star

--- questions/src/main/java/zero_50/backtrace/CombinationSumIII.java
+++ questions/src/main/java/zero_50/backtrace/CombinationSumIII.java
@@ -10,23 +10,25 @@ import java.util.List;
 public class CombinationSumIII {
     public List<List<Integer>> res = new ArrayList<>();
     public List<Integer> item = new ArrayList<>();
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-        backTrace(1,k,n);
+        backTrace(1, k, n);
         return res;
     }
+
     // begin[1,10] k = 个数 n为总和
-    public void backTrace(int begin,int k,int n){
-        if(k==item.size() && n==0){
+    public void backTrace(int begin, int k, int n) {
+        if (k == item.size() && n == 0) {
             res.add(new ArrayList<>(item));
             return;
         }
-        if (n<0){
+        if (n < 0) {
             return;
         }
-        for (int i = begin; i <10 ; i++) {
+        for (int i = begin; i < 10; i++) {
             item.add(i);
-            backTrace(i+1,k,n-i); // 这样保证了begin是升序，相当于剪枝了
-            item.remove(item.size()-1);
+            backTrace(i + 1, k, n - i); // 这样保证了begin是升序，相当于剪枝了
+            item.remove(item.size() - 1);
         }
     }
 }
