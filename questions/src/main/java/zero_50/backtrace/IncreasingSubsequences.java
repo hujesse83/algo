9b78fdb04ee4e2286ebd94bef8bb68475eaafbf5
hujesse83 <jesse.hu@liveramp.com>
2021-11-22 11:27:58 +0800

    递增子序列 + 数组hash去重 :star

--- questions/src/main/java/zero_50/backtrace/IncreasingSubsequences.java
+++ questions/src/main/java/zero_50/backtrace/IncreasingSubsequences.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * @Author huJesse
  * @Date 2021/11/21 18:12
- * @Description 求递增子集 + 去重的问题
+ * @Description 求递增子集 + 去重的问题 LC491
  * 输入：nums = [4,6,7,7]
  * 输出：[[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
  */
@@ -24,14 +24,14 @@ public class IncreasingSubsequences {
             res.add(new ArrayList<>(path));
         }
 
-        int[] used = new int[201]; // 保证了每一层的去重！妙啊 ，因为题目给定了-100<=nums[i]<=100
+        int[] used = new int[201]; // 保证了每一层的去重！妙啊 ，因为题目给定了-100<=nums[i]<=100 每一个栈都拥有一个used数组。利用数组替代哈希
         /**
          * 这里的去重与SubsetsII的去重又不一样了，前者是排好序的。而我没有排好序。
          */
         for (int i = start; i < nums.length; i++) {
             if (!path.isEmpty() && nums[i] < path.get(path.size() - 1) ||
                     (used[nums[i] + 100] == 1)) continue;
-            used[nums[i] + 100] = 1;
+            used[nums[i] + 100] = 1;  // 为什么需要 +100呢，当nums[i] 为-100的时候，对应hash表index为0的位置
             path.add(nums[i]);
             backtracking(nums, i + 1);
             path.remove(path.size() - 1);
