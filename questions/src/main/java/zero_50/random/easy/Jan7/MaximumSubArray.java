e6a5ec82ae83b626ff9724f90c2e5b271d8a3208
hujesse83 <jesse.hu@liveramp.com>
2022-01-07 15:59:41 +0800

    star

--- questions/src/main/java/zero_50/random/easy/Jan7/MaximumSubArray.java
+++ questions/src/main/java/zero_50/random/easy/Jan7/MaximumSubArray.java
@@ -1,7 +1,5 @@
 package zero_50.random.easy.Jan7;
 
-import java.util.Arrays;
-
 /**
  * @author jesse.hu
  * @date 2022/1/7 14:36
@@ -10,30 +8,31 @@ import java.util.Arrays;
 public class MaximumSubArray {
     // 方法一：贪心. 若当前指针所指元素的和小于0，则丢弃当前元素之前的序列.
     public int maxSubArray(int[] nums) {
-        if(nums.length==1){
+        if (nums.length == 1) {
             return nums[0];
         }
         int sum = Integer.MIN_VALUE;
         int count = 0;
-        for (int i = 0; i <nums.length ; i++) {
-            count+=nums[i];
-            sum = Math.max(sum,count);
-            if (count<0){
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
+            sum = Math.max(sum, count);
+            if (count < 0) {
                 count = 0;
             }
         }
-        return  sum;
+        return sum;
     }
+
     // 方法二、动态规格, 当前状态与上一个状态有关
     public int maxSubArrayDP(int[] nums) {
         int temp = nums[0];
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        for (int i = 1;i<nums.length;i++){
-            dp[i] = Math.max(nums[i],nums[i]+dp[i-1]);
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
             temp = Math.max(temp, dp[i]);  // stream 耗时较多.
         }
-       // return Arrays.stream(dp).max().getAsInt();  10ms -> 3ms
+        // return Arrays.stream(dp).max().getAsInt();  10ms -> 3ms
         return temp;
     }
 
