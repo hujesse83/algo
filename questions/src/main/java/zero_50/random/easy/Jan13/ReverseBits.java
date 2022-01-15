e36175b3d34417865f38305599c74eb2d64304ad
hujesse83 <jesse.hu@liveramp.com>
2022-01-15 23:23:37 +0800

    反转bit位

--- questions/src/main/java/zero_50/random/easy/Jan13/ReverseBits.java
+++ questions/src/main/java/zero_50/random/easy/Jan13/ReverseBits.java
@@ -18,7 +18,7 @@ public class ReverseBits {
     public static int reverseBits(int n) {
         int a=0;
         for(int i=0;i<=31;i++){
-            a=a+((1&(n>>i))<<(31-i));
+            a+=((1&(n>>i))<<(31-i));
         }
         return a;
     }
